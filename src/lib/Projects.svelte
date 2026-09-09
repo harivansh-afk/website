bc22b1e6734d99a9e5e1170f511a1edc50698217
Harivansh Rathi <rathiharivansh@gmail.com>
2026-09-09 22:04:39 +0000

    Link Draw to its GitHub repository

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -98,7 +98,7 @@
     },
     {
       name: "Draw",
-      href: "https://draw.harivan.sh",
+      href: "https://github.com/harivansh-afk/draw",
       media: "draw.mp4",
       width: 1718,
       height: 720,
